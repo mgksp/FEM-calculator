90e8b2d24d8f7d12008a798ed595383da8cced78
Prabu <prabu.mgks@gmail.com>
2022-06-21 17:56:38 +0530

    theme setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ module.exports = {
       fontFamily: {
         leagueSpartan: ["League Spartan", "sans-serif"],
       },
+      colors: {
+        main: "hsl(var(--color-main-bg) / <alpha-value>)",
+        keypad: "hsl(var(--color-keypad) / <alpha-value>)",
+        screen: "hsl(var(--color-screen) / <alpha-value>)",
+
+        primaryKey: "hsl(var(--color-primary-key) / <alpha-value>)",
+        primaryKeyShadow:
+          "hsl(var(--color-primary-key-shadow) / <alpha-value>)",
+        secondaryKey: "hsl(var(--color-secondary-key) / <alpha-value>)",
+        secondaryKeyShadow:
+          "hsl(var(--color-secondary-key-shadow) / <alpha-value>)",
+        accentKey: "hsl(var(--color-accent-key) / <alpha-value>)",
+        accentKeyShadow: "hsl(var(--color-accent-key-shadow) / <alpha-value>)",
+
+        text1: "hsl(var(--color-text1) / <alpha-value>)",
+        text2: "hsl(var(--color-text2) / <alpha-value>)",
+        text3: "hsl(var(--color-text3) / <alpha-value>)",
+      },
     },
   },
   plugins: [],
